1d8e4af1c5bbc716f70abe8cfddc68f67f4cd1c0
djangoreactdev <djangoreactdev@gmail.com>
2023-07-06 02:23:05 +0000

    asdfasdfasdf

--- jenkins/script.groovy
+++ jenkins/script.groovy
@@ -37,13 +37,15 @@ def deployApp() {
             writeFile file: '.env', text: readFile(env_test_aws)
         }
 
+
     def shellCmd = "bash ./server-cmds.sh ${IMAGE_django_web} ${IMAGE_nginx_proxy}"
+    def chmodfiles = "chmod +r .env && chmod +r server-cmds.sh"
     def ec2instans = 'ubuntu@35.173.231.122'
     sshagent(['ec2-jekins']) {
         sh "scp .env ${ec2instans}:/home/ubuntu"
         sh "scp docker-compose.prod-deploy.yml ${ec2instans}:/home/ubuntu"
         sh "cd jenkins && scp server-cmds.sh ${ec2instans}:/home/ubuntu"
-        sh "ssh -o StrictHostKeyChecking=no ${ec2instans} ${shellCmd}"
+        sh "ssh -o StrictHostKeyChecking=no ${ec2instans} ${chmodfiles} ${shellCmd}"
     }
 } 
 
--- jenkins/server-cmds.sh
+++ jenkins/server-cmds.sh
@@ -3,7 +3,5 @@
 export IMAGE_django_web=$1
 export IMAGE_nginx_proxy=$2
 
-chmod +r .env
-
 docker-compose -f docker-compose.prod-deploy.yml up --detach
 echo "success"
