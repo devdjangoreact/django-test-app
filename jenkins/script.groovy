5b817be37cccf3e98bac8bafa3d075f5bab42f93
djangoreactdev <djangoreactdev@gmail.com>
2023-07-06 03:09:45 +0000

    adsfafdasf

--- jenkins/script.groovy
+++ jenkins/script.groovy
@@ -38,15 +38,15 @@ def deployApp() {
         }
 
 
-    def shellCmd = "chmod +r .env && export IMAGE_django_web=${IMAGE_django_web} \
+    def shellCmd = "chmod +r -R app && cd /app && export IMAGE_django_web=${IMAGE_django_web} \
     && export IMAGE_nginx_proxy=${IMAGE_nginx_proxy} \
     && docker compose -f docker-compose.prod-deploy.yml build \
     && docker compose -f docker-compose.prod-deploy.yml up -d"
     def ec2instans = 'ubuntu@35.173.231.122'
     sshagent(['ec2-jekins']) {
-        sh "scp -o StrictHostKeyChecking=no .env ${ec2instans}:/home/ubuntu"
-        sh "scp -o StrictHostKeyChecking=no docker-compose.prod-deploy.yml ${ec2instans}:/home/ubuntu"
-        sh "cd jenkins && scp -o StrictHostKeyChecking=no server-cmds.sh ${ec2instans}:/home/ubuntu"
+        sh "scp -o StrictHostKeyChecking=no .env ${ec2instans}:/home/ubuntu/app"
+        sh "scp -o StrictHostKeyChecking=no docker-compose.prod-deploy.yml ${ec2instans}:/home/ubuntu/app"
+        sh "cd jenkins && scp -o StrictHostKeyChecking=no server-cmds.sh ${ec2instans}:/home/ubuntu/app"
         sh "ssh -o StrictHostKeyChecking=no ${ec2instans} ${shellCmd}"
     }
 } 
