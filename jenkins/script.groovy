ad547f2e2f8339841ce3454da3e1a3e4a786701e
djangoreactdev <djangoreactdev@gmail.com>
2023-07-06 01:16:00 +0000

    sdafadf

--- jenkins/script.groovy
+++ jenkins/script.groovy
@@ -42,8 +42,9 @@ def deployApp() {
     def dockerimage='docker compose -f docker-compose.prod.yml up -d --build'
     def ec2instans = 'ubuntu@35.173.231.122'
     sshagent(['ec2-jekins']) {
-        sh "scp .env ${ec2instans}:/home/ubuntu  && chmod 777 .env"
+        sh "scp .env ${ec2instans}:/home/ubuntu && chmod 777 .env"
         sh "scp docker-compose.prod.yml ${ec2instans}:/home/ubuntu"
+        sh 'ls -a'
         sh "ssh -o StrictHostKeyChecking=no ${ec2instans} ${dockerimage}"
     }
 } 
