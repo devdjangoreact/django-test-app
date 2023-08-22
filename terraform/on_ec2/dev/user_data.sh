b4d8d2efd7477efacd800940b7efaa564ef399cf
djangoreactdev <djangoreactdev@gmail.com>
2023-08-22 23:38:02 +0000

    push all

--- terraform/on_ec2/dev/user_data.sh
+++ terraform/on_ec2/dev/user_data.sh
@@ -28,3 +28,4 @@ sudo usermod -aG docker ec2-user
 sudo curl -L https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 docker-compose version
+
